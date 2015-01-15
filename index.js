a1bfc1b27de9ef5baf062518dee1e96f2b5cd985
streetlight <nweingartner@gmail.com>
2015-01-14 20:19:19 -0500

    Finish MVP

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var request = require("request"),
-	_ = require('underscore'),
-	twitter = require('twode'),
-	twit = new twitter({
-		consumer_key: 'tVdNWoXlrFZhU7sEowczFv4Sd',
-		consumer_secret: 'aYUL4iW9hAKG5Xb4EPTCL3UgKWhwmxuP3dYVY0RSuWCqgdrE7D',
-		access_token_key: '82392495-eoDRODmVwmye7IKb0VTe89QrwIvu7X76ZuxQLspvM',
-		access_token_secret: 'XTM4cmF1PlSdixf30Y658uaNWlo7EmGkf0Iukq8HAUFzh'
-	});
+    _ = require('underscore'),
+    twitter = require('twode'),
+    twit = new twitter({
+        consumer_key: 'Your',
+        consumer_secret: 'Keys',
+        access_token_key: 'Go',
+        access_token_secret: 'Here'
+    });
 
 var url = "https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json",
-	memory = {};
+    memory = {};
 
 function getStream(callback) {
-	request({
-	    url: url,
-	    json: true
-	}, function (error, response, body) {
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
 
-	    if (!error && response.statusCode === 200) {
-	    	
-	    	if (!_.isEqual(body, memory)) {1
-	    		console.log('hey hey hey');
-	    		_.each(body, function (item) {
-	    			twit.updateStatus();
-	    		});
-	    		
-	    		
-	    		memory = body;
-	    	}
-	    }
-	})
+        if (!error && response.statusCode === 200) {
+            
+            if (!_.isEqual(body, memory)) {1
+                _.each(body, function (item) {
+                    if (!_.where(memory, item).length) {
+                        // twit.updateStatus([item.title, item.url].join(" "), function (err, data) {
+                        //     if (!err) {
+                        //         console.log([item.title,"has been posted"].join(" "));
+                        //     }
+                        // });
+                    }
+                    
+                });
+
+                memory = body;
+            }
+        }
+    })
 };
 
 setInterval(getStream, 1000)
